6cae5848cd6009e5d08b3034288c13dc2962d435
CompulsiveCoder <compulsivecoder@gmail.com>
2017-11-19 03:49:12 +0000

    Tweaks to travis.yml and scripts

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,16 @@
-sudo apt-get update && apt-get -y upgrade
+# update
+sudo apt-get update &&
 
-sudo apt-get install -y curl python
+# python
+sudo apt-get install -y curl python &&
 
+# platform.io
 sudo python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/develop/scripts/get-platformio.py)"
+
+# Or
+
+#pip install -U platformio
+
+# TODO: Remove if not needed
+# docker
+#sudo curl -sSL https://get.docker.com/ | sudo sh
